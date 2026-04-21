da0c59a9f9b9678eaab32a3a9c31b67895619b5e
balderramanereida-coder <balderramanereida@gmail.com>
2026-04-21 21:53:13 +0000

    I learn how to name variables and use comments

---
+++ naming_variables.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+ int number;
+ //i delated the exclamation mark because if we follow the first rule, we will know that only letters are alowed
+int test_value;
+//I add a "_" because that´s how the structure to the variable creation works
+int example1;
+//if we want to reate more than one variable of the same type at a time we need to listing them with commas between their names, so if we want to reffer to a variable the number needs to be after the indicated variable
+float Uriel;
+//we delate the "int" and replaced with a different name 
+printf("Variables created successfully.\n");
+}
