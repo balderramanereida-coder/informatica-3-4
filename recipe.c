97b452c6f03e05a913a081552b1a7368f770b6c8
balderramanereida-coder <balderramanereida@gmail.com>
2026-04-25 04:40:39 +0000

    vertical and horizontal tabs

--- recipe.c
+++ recipe.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
-int main(void){
+int main(void)
+{
     printf("Coconut Cranberry Dessert\n");
-    printf(\n);
-
+    printf("\n");
+    printf("1 1/2 cups   \tGraham cracker crumbs\n");
+    printf("1/2 cup      \tbutter or margarine, melted\n");
+    printf("1 can        \tsweetened condensed milk\n");
+    printf("1 cup        \tflaked coconut\n");
+    printf("1 cup        \tdried cranberries\n");
+    printf("2 cups       \tvanilla chips\n");
+    printf("1 cup        \tsliced almonds or pecans");
+    printf("\n");
+    printf("\v1.Combine graham cracker crumbs and butter.Press into a greased 13″ x 9″ (33 x 23-cm) pan.\n");
+    printf("\v2.Combine remaining ingredients and spread over graham cracker mixture. Bake at 350°F (175°C) for 25 minutes or until the edges are golden brown. Let cool and cut into squares.\n");
 }
