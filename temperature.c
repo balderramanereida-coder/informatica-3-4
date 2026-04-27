98e0b1bc869a706b9cf3554587f1fd26c4c2fb79
balderramanereida-coder <balderramanereida@gmail.com>
2026-04-27 17:20:34 +0000

    convertion of temperature

--- temperature.c
+++ temperature.c
@@ -2,7 +2,10 @@
 
 int main(void)
 {
-    float X;
-    printf("%fF, "))
-
+    float F;
+printf("Enter temperature(Farhenheit): ");
+scanf("%f",&F);
+float C=(F-32)/1.8;
+    printf("%fF°= %fC°\n",F,C);
+    //the number that is equal in F° and C° is= -40
 }
