f4177993ec25dc7a999dcac9088ea6ecb43a6320
balderramanereida-coder <balderramanereida@gmail.com>
2026-04-28 21:58:59 +0000

    minutes

---
+++ minutes.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+    float m;
+    printf("Movie running time (minutes): ");
+    scanf("%f",&m);
+    float h=(m/60);
+    printf("Start time: ");
+    scanf("%f",&h);
+    printf("\n");
+    printf("The movie will last:%f hours",&m);
+    scanf("%f",&h);
+
+
+
+
+
+}
